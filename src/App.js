7634b031e7d56854efd0fa5b5f09de982779aea7
heeyonhh <lightram23@gmail.com>
2023-07-10 11:18:40 +0900

    01

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  //서버에서 가져온 데이터라고 생각하고
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* JSX : 리액트에서 쉽게 쓸수있는 문법*/}
+      <div className="black-nav">
+        {/* <h4 id={post}>블로그임</h4> */}
+        {/* css파일 안넣고 바로 style 넣을때 오브젝트 문법으로 */}
+        <h4 style={ {color : 'red', fontSize : '30px'} }>블로그임</h4>
+      </div>
+      {/* 데이터 바인딩 : 중괄호 문법 id className 상상하는 모든 곳에 넣을 수 있음 */}
+      <h4>{post}</h4>
     </div>
   );
 }
