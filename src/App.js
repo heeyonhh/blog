e591c656b18b703f7f59c255fe07b42263ce1e1a
heeyonhh <lightram23@gmail.com>
2023-07-11 13:13:03 +0900

    componets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-/*eslint-disable*/
-//lint (워닝 메세지) 끄는 기능
-
 import { useState } from 'react';
 import './App.css';
 
+//실은 컴포넌트임
 function App() {
 
-  let [a, b] = useState(['남자 코트 추천', '여자 신발 추천', '한남동 카페 추천']);
+  let [a, b] = useState(['남자 코트 추천', '여자 신발 추천', '한남동 카페 추천', '성수동 맛집 추천', '이달의 영화 추천']);
   let [따봉, 따봉변경] = useState(0);
-  //따봉변경 state변경 함수
-
-  // function 함수(){
-  //   console.log(1);
-  // };
 
   return (
     <div className="App">
@@ -20,29 +13,23 @@ function App() {
         <h4>HEE BLOG</h4>
       </div>
 
+      {/* 정렬 필터 */}
       <button onClick={()=>{
+        let copy = [...a];
+        copy.sort();
+        b(copy);
+      }}>정렬</button>
 
-        let arr = [1, 2, 3];
-        //array/object담은 변수엔 화살표만 저장됨 123은 미지의 공간 ram에 저장됨
-        //state변경함수 기존state = 신규state경우 변경 안해줌
-
-        //arry object다룰 때 원본은 보존하는게 좋음
-        //console.log(copy == a) true 그래서 변경이 안됨 기존 state에서 변경이 안됬다고 생각해서 : reference data type 더공부하기
-        //object도 마찬가지임 ... : 괄호 벗겨 주세요 괄호 씌워주세요 이러면 화살표도 달라짐 -> 새로운 스테이트로 이해해서 변경됨
-
-        //기존 state가 array, object data면 stata수정할때 독립적 카피본을 만들어서 수정하자! : shallow copy
+      <button onClick={()=>{
         let copy = [...a];
         copy[0] = '여자 코트 추천';
         b(copy);
       }}>글수정</button>
 
       <div className="list">
-        {/* onclick이벤트 핸들러 {}안에 함수 넣어야함 ()=>{} 함수 넣어도 됨*/}
-        {/* state변경 등호로 변경하면 안됨 */}
         <h4>{ a[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>🩵</span> {따봉} </h4>
         <p>7월 10일 발행</p>
       </div>
-
 
       <div className="list">
         <h4>{ a[1] }</h4>
@@ -52,8 +39,52 @@ function App() {
         <h4>{ a[2] }</h4>
         <p>7월 2일 발행</p>
       </div>
+      <div className="list">
+        <h4>{ a[3] }</h4>
+        <p>6월 15일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ a[4] }</h4>
+        <p>6월 7일 발행</p>
+      </div>
+
+      {/* html 깔끔하게 쓰기 */}
+      <Modal/>
+      {/* <Modal></Modal> */}
+
+      <Login/>
     </div>
   );
+};
+
+//컴포넌트 만들때 다른 펑션 바깥에 만들기 & 영어대문자
+//컴포넌트 : 반복적인 html 축약할때 / 페이지 전환 / 자주 변경되는 것들
+//컴포넌트를 만드는 것은 자유지만 책임이 따름 : 컴포넌트의 단점 state가져다 쓸때 문제 생김, 다른 펑션안에 있는 변수 쓸수 없음
+function Modal(){
+  return(
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    //의미 없는 <div>대신 <></>사용가능 : 프래그먼트 문법
+  );
+};
+
+//컴포넌트 문법 2
+// let Modal = () => {
+//   return(
+//     <div></div>
+//   )
+// }
+// const 로 하면 에러메세지 띄어주는 장점이 있음
+
+const Login = () => {
+  return(
+    <>
+      🐻‍❄️🐻‍❄️🐻‍❄️🐻‍❄️🐻‍❄️🩵🩵🩵🩵🩵👖👖👖👖👖
+    </>
+  )
 }
 
 export default App;
