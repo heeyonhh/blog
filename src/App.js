9016822b9942ad505a6d928519c2d49c0a2dbde8
heeyonhh <lightram23@gmail.com>
2023-07-12 18:29:43 +0900

    props 변경함수

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '여자 신발 추천', '한남동 카페 추천', '성수동 맛집 추천', '이달의 영화 추천']);
   let [따봉, 따봉변경] = useState([0,0,0,0,0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
   return (
     <div className="App">
@@ -19,7 +20,7 @@ function App() {
         글제목.map(function(a, i){
           return (
             <div className="list" key={i}>
-              <h4 onClick={()=>{setModal(true)}}>{ 글제목[i] } <span onClick={()=>{
+              <h4 onClick={()=>{setModal(!modal); setTitle(i) }}>{ 글제목[i] } <span onClick={()=>{
                 let copy = [...따봉];
                 copy[i] = copy[i] + 1;
                 따봉변경(copy)
@@ -30,8 +31,12 @@ function App() {
         })
       }
       
+      {/* <button onClick={()=>{ setTitle(0) }}>글제목0</button>
+      <button onClick={()=>{ setTitle(1) }}>글제목1</button>
+      <button onClick={()=>{ setTitle(2) }}>글제목1</button> */}
+
       {
-        modal == true ? <Modal 글제목변경={글제목변경} 글제목={글제목}/> : null
+        modal == true ? <Modal title={title} 글제목={글제목} /> : null
       }
     </div>
   );
@@ -39,13 +44,11 @@ function App() {
 
 function Modal(props){
   return(
-    <div className='modal' style={{background: props.color}}>
-      <h4 id='name'>{props.글제목[0]}</h4>
+    <div className='modal'>
+      <h4>{ props.글제목[props.title] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button onClick={()=>{
-        props.글제목변경(['여자코트 추천', '강남 맛집 추천', '파이썬 독학', '성수동 맛집 추천', '이달의 영화 추천'])
-      }}>글수정</button>
+      <button>글수정</button>
     </div>
   );
 };
